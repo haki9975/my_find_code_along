ee658d8506607246b750e890e1ebdf12c2bd3221
haki9975 <elias.robert.hakim@gmail.com>
2021-02-08 14:03:53 -0500

    commit tree efcb149a4dd25d7f8af3558cec0d9bf5bf9e3708

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
 def my_find(collection)
-
+    i = 0
+      while i < collection.length
+          yield(collection[i])
+      end
+    if collection[i] = true
+            collection[i]
+        
+        elsif collection[i] = false
+            i = i + 1
+    end
+  
 end
